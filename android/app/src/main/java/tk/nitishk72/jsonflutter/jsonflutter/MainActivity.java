395eea78a752b280b7b2c70a11cbcd969af8bfa0
Nitish Kumar Singh <nitishk72@gmail.com>
2018-04-20 20:32:25 +0530

    Initial commit

---
+++ android/app/src/main/java/tk/nitishk72/jsonflutter/jsonflutter/MainActivity.java
@@ -0,0 +1,14 @@
+package tk.nitishk72.jsonflutter.jsonflutter;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+main(){
+  runApp(new AppName());
+}
+
+class AppName extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new AppState();
+  }
+}
+
+class AppState extends State<AppName>{
+  String name = '';
+  int id = 0;
+  int public_repos = 0;
+  TextEditingController user = new TextEditingController();
+  String url = 'https://api.github.com/users/';
+
+  getData(String username) async{
+    String profile = url+username;
+    var res = await http.get(profile,headers: {"Accept":"application/json"});
+    var resBody = json.decode(res.body);
+    name = resBody['name'];
+    id = resBody['id'];
+    public_repos = resBody['public_repos'];
+    setState(() {
+      print("Success");
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar:  AppBar(
+          title: new Text("JSON App"),
+        ),
+        body: new Container(
+          padding: EdgeInsets.all(12.0),
+          child: new Column(
+            children: <Widget>[
+              new Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              new TextField(
+                controller: user,
+                decoration: InputDecoration(hintText: "Enter Github Uername"),
+              ),
+              new Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              new RaisedButton(
+                  onPressed: ()=>getData(user.text),
+                  child: new Text("Get Info")
+              ),
+              new Padding(padding: EdgeInsets.only(bottom: 20.0)),
+              new Text("Name : $name"),
+              new Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              new Text("ID : $id"),
+              new Padding(padding: EdgeInsets.only(bottom: 10.0)),
+              new Text("Public repo : $public_repos"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
